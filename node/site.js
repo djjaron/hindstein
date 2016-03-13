815a78484f70e65fde7e3ec9858f45131c5230cd
Will <will@willinchina.com>
2016-03-13 21:41:59 +0800

    Save to firebase

--- node/site.js
+++ node/site.js
@@ -12,8 +12,10 @@ var express = require('express'),
     server = require('http').Server(app),
     io = require('socket.io')(server),
     accountSid = 'AC7ebe34d91f75cad45e7f096d011f2d21',
-    authToken = '48e9253927058f8421e9a47aaac4c6fe', 
-    client = require('twilio')(accountSid, authToken); 
+    authToken = '48e9253927058f8421e9a47aaac4c6fe',
+    client = require('twilio')(accountSid, authToken),
+    Firebase = require('firebase'),
+    uuid = require('uuid-js');
 
 // allow access to all public files
 app.use(express.static(__dirname + '/public'));
@@ -35,33 +37,56 @@ io.on('connection', function (socket) {
     //------------------- SOCKETS ---------------------  
     // Welcome Message
     socket.on('phoneNumber', function (data) {
-        console.log('data: '+data);
-        console.log('data.phoneNumber'+data.phoneNumber);
+        console.log('data: ' + data);
+        console.log('data.phoneNumber' + data.phoneNumber);
         twillioSend(data.phoneNumber, '+1 424-231-2986', 'Welcome to Hindstein!', 'http://www.hindste.in/img/uploads/000000000001.gif');
     });
     
-     //------------------- TWILLIO --------------------- 
+    //------------------- TWILLIO --------------------- 
 
     function twillioSend(to, from, body, mediaUrl) {
-        console.log('To '+to);
-        console.log('From '+ from);
-        console.log('Body '+body);
-        console.log('mediaUrl '+mediaUrl);
-        
+        console.log('To ' + to);
+        console.log('From ' + from);
+        console.log('Body ' + body);
+        console.log('mediaUrl ' + mediaUrl);
+
         client.messages.create({
             to: to,
             from: from,
             body: body,
             mediaUrl: mediaUrl,
         }, function (err, message) {
-            console.log('------ ERROR ------');
-            console.log(err);
-            console.log('------ MESSAGE------');
-            console.log(message);
-            socket.emit('phoneNumber', { twillioError: 'err', twillioMessage: message});
-            // SAVE TO DATABASE
+
+            if (err) {
+                console.log('------ ERROR ------');
+                console.log(err);
+                socket.emit('phoneNumber', { twillioError: 'err' });
+            } else {
+                console.log('------ MESSAGE------');
+                console.log(message);
+                socket.emit('phoneNumber', { twillioMessage: message });
+                savePhoneNumber(to);
+            }
         });
     }
+    
+    
+    //------------------- UUID --------------------
+    
+    function makeID() {
+        return uuid.create().toString();
+    }
   
+    //------------------- FIREBASE --------------------- 
+    
+    //-- SAVE PHONE NUMBER
+    function savePhoneNumber(phoneNumber) {
+        var id = makeID();
+        var myFirebaseRef = new Firebase("https://hindstein.firebaseio.com/hindstein/phoneNumbers/" + id);
+        myFirebaseRef.set({
+            phoneNumber: phoneNumber         
+        });
+    }
+       
     //------------------- END ---------------------  
 });
