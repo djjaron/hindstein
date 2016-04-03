b1d93ffe6834ce5f540ce01fa23f8c8ac0a696a8
@bkawk <email.will.in.china@gmail.com>
2016-04-03 17:54:33 +0800

    car media

--- node/site.js
+++ node/site.js
@@ -59,7 +59,7 @@ io.on('connection', function (socket) {
         }) 
         .then(function(result) {
             // console.log('Sending: '+ result.val()); 
-             twillioSend("+14243747066",  "+17027488799", 'Test02', "https://s3-us-west-2.amazonaws.com/img.hindste.in/welcome.png")
+             twillioSend("+14243747066",  "+17027488799", 'Test02', "https://www.enterprise.com/content/dam/global-vehicle-images/cars/FORD_FOCU_2012-1.png")
              
         });        
     });
